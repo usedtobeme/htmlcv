97df3f2d11412231bbe431855556328bf1b0f654
Ivan Amortegui <Ivan.Amortegui@bizagi.com>
2017-09-21 17:56:25 -0500

    Added Edit Experience file

---
+++ src/components/EditExperience.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form>
+      <ul>
+        <li :v-for="experience" :key="id">
+          <label>Company:</label>
+          <input :value="company" @input="updateCompany">
+          <label>Start Time:</label>
+          <input type="date" :value="startTime" @input="updateStartTime">
+          <label>End time:</label>
+          <input type="date" :value="endTime" @input="updateEndTime">
+          <label>Position:</label>
+          <input :value="position" @input="updatePosition">
+          <label>Description:</label>
+          <textarea :value="description" @input="updateDescription"></textarea>
+
+        </li>
+      </ul>
+    </form>
+  </div>
+</template>
